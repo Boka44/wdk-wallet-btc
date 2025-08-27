2b0b63d65e5ce0dc386e71999055cbd8f4f1e147
AlonzoRicardo <alonzoricardo6@gmail.com>
2025-08-26 20:14:29 -0400

    use 1 worker to guarantee no race conditions and force exit to mitigate tcp connections not properly closing TCPWRAP

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,7 @@ export default {
   globalSetup: './tests/jest/jest.setup.js',
   globalTeardown: './tests/jest/jest.teardown.js',
   testEnvironment: 'node',
-  testTimeout: 60000
+  testTimeout: 60000,
+  maxWorkers: 1,
+  forceExit: true
 }
